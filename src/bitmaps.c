c4343e41793dc1ea0363daa18ebec214905e9313
Klas Björkqvist <klas.bjorkqvist@gmail.com>
2014-06-06 13:12:17 +0200

    Memoization on size calculations and refactoring.
    Memoize the result of size calculations for clock styles.
    Refactoring of the bitmap creation and deletion functions.

--- src/bitmaps.c
+++ src/bitmaps.c
@@ -1,9 +1,16 @@
 #include "bitmaps.h"
 #include <pebble.h>
 
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+
+/*
+ * Memoization structures
+ */
 static GBitmap* nums_bmps[NUMS_LAST][10] = { {NULL,}, };
 
 static GBitmap* texts_bmps[TEXTS_LAST] = { NULL,};
+
+static struct { GSize size; bool valid; } total_sizes[STYLE_LAST] = { {{0},false}, };
 
 
 /*
@@ -68,51 +75,49 @@ static const ResourceId resid_nums[NUMS_LAST][10] = {
   }
 };
 
-static GBitmap* load(GBitmap* *bmp, ResourceId id) {
-  if (*bmp == NULL) {
-    *bmp = gbitmap_create_with_resource(id);
-  }
-  return *bmp;
-}
+/*
+ * Helper functions
+ */
+
+static GBitmap* bitmaps_create(GBitmap* *bmp, ResourceId id);
+static void bitmaps_destroy(GBitmap *bmp);
+static void bitmaps_draw(GContext *ctx, GBitmap* *bmp, ResourceId id, GPoint p);
+static void set_two_digit_string(int *str, int num);
+
+/*
+ * Externally accessible functions
+ */
 
 GSize bitmaps_get_size_num(Nums n) {
   /* TODO: Check return value */
-  GBitmap* b = load(&nums_bmps[n][0], resid_nums[n][0]);
+  GBitmap* b = bitmaps_create(&nums_bmps[n][0], resid_nums[n][0]);
   return b->bounds.size;
 }
 
 GSize bitmaps_get_size_text(Texts t) {
   /* TODO: Check return value */
-  GBitmap* b = load(&texts_bmps[t], resid_texts[t]);
+  GBitmap* b = bitmaps_create(&texts_bmps[t], resid_texts[t]);
   return b->bounds.size;
 }
 
-#define MAX(a,b) ((a) > (b) ? (a) : (b))
-
 GSize bitmaps_get_size_clock(Style style) {
-  GSize total_size = {0,0};
+  if (total_sizes[style].valid) return total_sizes[style].size;
+
+  total_sizes[style].valid = true;
+  GSize *total_size = &total_sizes[style].size;
   GSize current_size = {0,0};
 
   for (int i = 0; i < 6; ++i) {
     if (number_styles[style][i] == NONE) continue;
     current_size = bitmaps_get_size_num(number_styles[style][i]);
-    total_size.h = MAX(total_size.h, current_size.h);
-    if (total_size.w > 0) {
-      total_size.w += 1 + ((i+1) % 2);
+    total_size->h = MAX(total_size->h, current_size.h);
+    if (total_size->w > 0) {
+      total_size->w += 1 + ((i+1) % 2);
     }
-    total_size.w += current_size.w;
+    total_size->w += current_size.w;
   }
 
-  return total_size;
-}
-
-static void load_and_draw(GContext *ctx, GBitmap* *bmp, ResourceId id, GPoint p) {
-  /* TODO: Check return value */
-  load(bmp, id);
-
-  GRect bounds = (*bmp)->bounds;
-
-  graphics_draw_bitmap_in_rect(ctx, *bmp, (GRect) { .origin = p, .size = bounds.size });
+  return *total_size;
 }
 
 void bitmaps_init() {
@@ -131,26 +136,73 @@ void bitmaps_init() {
 void bitmaps_deinit() {
   /* deinit nums */
   for (int i = 0; i < NUMS_LAST; ++i)
-    for (int j = 0; j < 10; ++j)
-      if (nums_bmps[i][j] != NULL) {
-        gbitmap_destroy(nums_bmps[i][j]);
-        nums_bmps[i][j] = NULL;
-      }
+    for (int j = 0; j < 10; ++j) {
+      bitmaps_destroy(nums_bmps[i][j]);
+      nums_bmps[i][j] = NULL;
+    }
 
   /* deinit texts */
-  for (int i = 0; i < TEXTS_LAST; ++i)
-    if (texts_bmps[i] != NULL) {
-      gbitmap_destroy(texts_bmps[i]);
+  for (int i = 0; i < TEXTS_LAST; ++i) {
+      bitmaps_destroy(texts_bmps[i]);
       texts_bmps[i] = NULL;
-    }
+  }
 }
 
 void bitmaps_draw_text(GContext *ctx, Texts t, GPoint p) {
-  load_and_draw(ctx, &texts_bmps[t], resid_texts[t], p);
+  bitmaps_draw(ctx, &texts_bmps[t], resid_texts[t], p);
 }
 
 void bitmaps_draw_num(GContext *ctx, Nums n, int i, GPoint p) {
-  load_and_draw(ctx, &nums_bmps[n][i], resid_nums[n][i], p);
+  bitmaps_draw(ctx, &nums_bmps[n][i], resid_nums[n][i], p);
+}
+
+void bitmaps_draw_clock(GContext *ctx, Style style, GPoint tl, struct tm *t) {
+  int nums[6];
+  GSize total_size = bitmaps_get_size_clock(style);
+
+  set_two_digit_string(&nums[0], t->tm_hour);
+  set_two_digit_string(&nums[2], t->tm_min);
+  set_two_digit_string(&nums[4], t->tm_sec);
+
+  int x = tl.x;
+  for (int i = 0; i < 6; ++i) {
+    if (number_styles[style][i] == NONE) continue;
+    GSize size = bitmaps_get_size_num(number_styles[style][i]);
+    if (x > tl.x) {
+      x += 1 + ((i+1) % 2);
+    }
+    bitmaps_draw_num(ctx, number_styles[style][i], nums[i],
+                     (GPoint){ .x = x, .y = tl.y + total_size.h - size.h });
+    x += size.w;
+  }
+
+}
+
+
+/*
+ * Helper function implementations
+ */
+
+static GBitmap* bitmaps_create(GBitmap* *bmp, ResourceId id) {
+  if (*bmp == NULL) {
+    *bmp = gbitmap_create_with_resource(id);
+  }
+  return *bmp;
+}
+
+static void bitmaps_destroy(GBitmap *bmp) {
+  if (bmp != NULL) {
+    gbitmap_destroy(bmp);
+  }
+}
+
+static void bitmaps_draw(GContext *ctx, GBitmap* *bmp, ResourceId id, GPoint p) {
+  /* TODO: Check return value */
+  GBitmap* b = bitmaps_create(bmp, id);
+
+  GRect bounds = b->bounds;
+
+  graphics_draw_bitmap_in_rect(ctx, b, (GRect) { .origin = p, .size = bounds.size });
 }
 
 static void set_two_digit_string(int *str, int num) {
@@ -162,28 +214,3 @@ static void set_two_digit_string(int *str, int num) {
   str[1] = num%10;
 }
 
-void bitmaps_draw_clock(GContext *ctx, Style style, GPoint tl, struct tm *t) {
-  int nums[6];
-  int height[STYLE_LAST] = {
-    [STYLE_LARGE] = 30,
-    [STYLE_LARGE_MINUTE] = 30,
-    [STYLE_SMALL] = 14,
-    [STYLE_SMALL_HM] = 14,
-    [STYLE_SMALL_MS] = 14
-  };
-
-  set_two_digit_string(&nums[0], t->tm_hour);
-  set_two_digit_string(&nums[2], t->tm_min);
-  set_two_digit_string(&nums[4], t->tm_sec);
-
-  int x = tl.x;
-  for (int i = 0; i < 6; ++i) {
-    if (number_styles[style][i] == NONE) continue;
-    if (x > tl.x) {
-      x += 1 + ((i+1) % 2);
-    }
-    bitmaps_draw_num(ctx, number_styles[style][i], nums[i], (GPoint){ .x = x, .y = tl.y + height[style] - num_sizes[number_styles[style][i]].h });
-    x += num_sizes[number_styles[style][i]].w;
-  }
-
-}
